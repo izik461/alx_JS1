66089beab8a9aff7a14be258b4079103a3ea9445
Jerzy Świniarski <jerzyk@codequest.com>
2022-03-20 10:59:17 +0100

    Eslinted!

--- zadanie1/src/components/index.jsx
+++ zadanie1/src/components/index.jsx
@@ -2,6 +2,9 @@ import React from "react"; // - składnia JS Modules
 // const React = require('react') - składnia Common.JS - wymusza type="module" w index.html
 import ReactDOM from "react-dom";
 
-alert("Hello world from index.js");
+// alert("Hello world from index.js");
 
-ReactDOM.render(<div>Hello world from reactDom.render</div>, document.querySelector("#app"));
+ReactDOM.render(
+  <div>Hello world from reactDom.render</div>,
+  document.querySelector("#app")
+);
