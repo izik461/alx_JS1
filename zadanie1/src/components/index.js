8ef107a3bd170dfc8bb729cc9d3623d79e000416
Jerzy Świniarski <jerzyk@codequest.com>
2022-03-20 10:50:11 +0100

    Added dist to gitignore

--- zadanie1/src/components/index.js
+++ zadanie1/src/components/index.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React from "react"; // - składnia JS Modules
+// const React = require('react') - składnia Common.JS - wymusza type="module" w index.html
 import ReactDOM from "react-dom";
 
 alert("Hello world from index.js");
+
+ReactDOM.render(<div>Hello world from reactDom.render</div>, document.querySelector("#app"));
