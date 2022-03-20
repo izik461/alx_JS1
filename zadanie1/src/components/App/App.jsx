357b851aacda5411c8c0db405eb515085a900342
Jerzy Świniarski <jerzyk@codequest.com>
2022-03-20 16:27:52 +0100

    TODO lista działa

--- zadanie1/src/components/App/App.jsx
+++ zadanie1/src/components/App/App.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import TodoList from "../TodoList/TodoList";
 
+import styles from "./App.module.css";
+
 export const App = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
+  const [showError, setShowError] = useState(false);
+  // const [errorMessage, setErrorMessage] = useState("");
 
   // useEffect(() => {}, [todos]); // uruchomienie przy kazdej zmianie todos
   useEffect(() => {
@@ -14,7 +18,7 @@ export const App = () => {
   }, []);
 
   const handleInputChange = (event) => {
-    console.log("App.jsx HandleInputChange fired: " + event.target.value);
+    // console.log("App.jsx HandleInputChange fired: " + event.target.value);
     setInputValue(event.target.value);
   };
 
@@ -23,9 +27,13 @@ export const App = () => {
     event.preventDefault();
     const currentText = inputValue;
     if (currentText.length < 2) {
-      alert("Input not valid. Length<2");
+      // alert("Input not valid. Length<2");
+      // setErrorMessage("Invalid input. Length<2");
+      setShowError(true);
       return;
     }
+    // setErrorMessage("");
+    setShowError(false);
     const newTodos = [
       ...todos,
       {
@@ -52,6 +60,9 @@ export const App = () => {
           Send todo
         </button>
       </form>
+      {showError ? (
+        <p className={styles.error}>Invalid input. :( :( :(</p>
+      ) : null}
       <TodoList todoList={todos} />
     </div>
   );
--- zadanie1/src/components/TodoList/TodoList.jsx
+++ zadanie1/src/components/TodoList/TodoList.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
+import styles from "./ToDoList.module.css";
+
 const TodoList = (props) => {
   return (
     <ul>
-      {/* <li>Some sample element</li> */}
       {props.todoList.map((todo, index) => {
-        return <li key={index}>{todo.name}</li>;
+        const randomPriority = Math.floor(Math.random() * 3);
+        let priorityString = "minor";
+        switch (randomPriority) {
+          case 0:
+            priorityString = "minor";
+            break;
+          case 1:
+            priorityString = "major";
+            break;
+          default:
+            priorityString = "critical";
+            break;
+        }
+        console.log(
+          "using priorityString: " + randomPriority + " " + priorityString
+        );
+        // const className = "styles." + priorityString;
+        // console.log("using className: " + className);
+        return (
+          <li key={index} className={styles[priorityString]}>
+            {todo.name}
+          </li>
+        );
       })}
     </ul>
   );
