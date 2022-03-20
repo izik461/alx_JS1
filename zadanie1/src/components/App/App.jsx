09a4aa0c0b2afe20dc2b0239c52254b18f287558
Jerzy Świniarski <jerzyk@codequest.com>
2022-03-20 11:33:51 +0100

    Added App.jsx

---
+++ zadanie1/src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = (
+  <div>
+    <h1>TODO LIST:</h1>
+    <form>
+      <input type="text" placeholder="Add a todo" />
+      <button type="submit">Send todo</button>
+    </form>
+  </div>
+);
--- zadanie1/src/components/index.jsx
+++ zadanie1/src/components/index.jsx
@@ -2,9 +2,12 @@ import React from "react"; // - składnia JS Modules
 // const React = require('react') - składnia Common.JS - wymusza type="module" w index.html
 import ReactDOM from "react-dom";
 
+import { App } from "./App/App";
+
 // alert("Hello world from index.js");
 
 ReactDOM.render(
-  <div>Hello world from reactDom.render</div>,
-  document.querySelector("#app")
+  App,
+  // <div>Hello world from reactDom.render</div>, // jaki html wklejamy
+  document.querySelector("#app") // i gdzie
 );
