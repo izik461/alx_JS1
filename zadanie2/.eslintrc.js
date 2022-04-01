d2291b584fc16651751240c70c3bdbb8ec525979
Jerzy Świniarski <jerzyk@codequest.com>
2022-04-01 15:04:01 +0200

    Created empty Chat module

---
+++ zadanie2/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+  },
+};
---
+++ zadanie2/src/components/App/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Twoim zadaniem jest napisanie aplikacji typu chat, skladajacej sie z 2 komponentow: App i Chat.
+
+// App jest to rodzic, ktory ma w sobie komponent chat.
+// App renderuje formularz, ktory ma 2 pola: person i message
+
+// Dane niech sie zapisuja do localStorage
+
+// Po wypelnieniu formularza, niech dane dopisza sie do chatu (przekaz to przez propsy)
+
+// Zrob walidacje, ze pole person nie moze byc puste
+// Zrob walidacje, ze pole message nie moze byc puste
+
+// Postaraj sie to zadanie ostylowac
+// mniej wiecej https://www.chip.pl/wp-content/uploads/2008/11/4e8ac7a1d3587e40c02c2333c9e1365e.jpg
+
+function App() {
+  return <div>hello world</div>;
+}
+
+export default App;
