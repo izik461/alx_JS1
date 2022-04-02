185753d5537343b12b746f829d25e2a7ceb6149b
Jerzy Świniarski <jerzyk@codequest.com>
2022-04-02 16:50:04 +0200

    Get working - reading from file

--- zadanie5-nodeServer/server.js
+++ zadanie5-nodeServer/server.js
@@ -1,16 +1,30 @@
 import http from 'http';
+import fs from 'fs';
 
 console.log('Server.js hello world')
+
+const fsp = fs.promises;
 
 const server = http.createServer((request, response) => {
   console.log('hello', request.url)
 
   if (request.url === '/cars') {
     if (request.method === 'GET') {
-      response.writeHead(200, { contentType: "application/json" })
-      // const response = "response from server!" + Date.now()
-      response.write(JSON.stringify({ cars: [] }))
-      return response.end()
+
+      return fsp.readFile('./data/cars.json', 'utf8')
+        .then(jsonFile => {
+          console.log(jsonFile);
+          response.writeHead(200, { contentType: "application/json" })
+          response.write(jsonFile)
+          return response.end()
+        })
+        .catch(error => {
+          console.log('Error reading file: ', error)
+
+          response.writeHead(500, { contentType: "application/json" })
+          response.write(JSON.stringify({ messsage: error }))
+          return response.end()
+        })
     }
   }
 
