272250217489a269b6bed8e08e69d1d65a011233
Jerzy Świniarski <jerzyk@codequest.com>
2022-04-03 10:36:55 +0200

    Reading/writing to file

--- zadanie6-express/server.js
+++ zadanie6-express/server.js
@@ -1,13 +1,36 @@
 import express from 'express'
+import fs from 'fs'
 
-console.log('Hello server.js')
+console.log('Hello from server.js')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+
+  fs.readFile('./data/hello.txt',
+    'utf8',
+    (error, data) => {
+      if (error) {
+        console.error(error)
+        return
+      }
+      console.log(data)
+
+      fs.writeFile('./data/hello2.txt',
+        data,
+        'utf8',
+        (error2, data2) => {
+          if (error2) {
+            console.log(error2)
+            return
+          }
+          console.log('Created hello2.txt')
+        })
+    })
+
+  res.send('Hello world')
 })
 
-app.listen(5002, () => {
-  console.log('Express server running on port 5002');
+app.listen(5001, () => {
+  console.log('Express server srunning on port 5001')
 })
