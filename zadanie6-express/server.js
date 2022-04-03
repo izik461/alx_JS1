490dac13ae0371e221fa4d4a4dd84a32331887f4
Jerzy Świniarski <jerzyk@codequest.com>
2022-04-03 09:58:20 +0200

    postawiony sample express

--- zadanie6-express/server.js
+++ zadanie6-express/server.js
@@ -1,1 +1,13 @@
+import express from 'express'
+
 console.log('Hello server.js')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
+
+app.listen(5002, () => {
+  console.log('Express server running on port 5002');
+})
