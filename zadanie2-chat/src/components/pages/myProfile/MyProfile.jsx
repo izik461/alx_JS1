2f6322141c6289f27835c8fb18e5094114eb9258
Jerzy Świniarski <jerzyk@codequest.com>
2022-04-23 15:35:31 +0200

    Empty myProfile view

--- zadanie2-chat/src/components/pages/myProfile/MyProfile.jsx
+++ zadanie2-chat/src/components/pages/myProfile/MyProfile.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Main from 'components/layouts/main/Main';
+import InputGroup from 'components/elements/inputGroup/InputGroup';
 
 function MyProfile() {
-  return <Main>Hello My profile</Main>;
+  const [nameValue, setNameValue] = useState('');
+  const handleValueChanged = (event) => {
+    console.log(`Handle Change in MyProfile.jsx: ${event.target.value}`);
+    setNameValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Tapped submit with name provided: ${nameValue}`);
+    // const newMessageId = Date.now();
+
+    // save('/', {
+    //   id: newMessageId,
+    //   person: personInputValue,
+    //   message: messageInputValue,
+    // });
+
+    // setPersonInputValue('');
+    // setMessageInputValue('');
+  };
+
+  return (
+    <Main>
+      <form onSubmit={handleSubmit}>
+        <InputGroup
+          id="Name"
+          type="text"
+          label="Name label"
+          handleChange={handleValueChanged}
+          inputValue={nameValue}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </Main>
+  );
 }
 
 export default MyProfile;
