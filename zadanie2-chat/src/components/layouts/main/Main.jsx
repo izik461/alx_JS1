725d50060c5b83a51f433b17895fe37fc0b337d9
Jerzy Świniarski <jerzyk@codequest.com>
2022-04-23 14:51:05 +0200

    Styling.
    MyProfile page created

--- zadanie2-chat/src/components/layouts/main/Main.jsx
+++ zadanie2-chat/src/components/layouts/main/Main.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './style.module.css';
 
-function MainLayout({ children }) {
+function Main({ children }) {
   return (
-    <>
-      <header>Hello header - main.jsx</header>
-      <div className="content">{children}</div>
-      <footer>Hello footer</footer>
-    </>
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <a href="/" className={styles.logo}>
+          <h1 className={styles.title}>Chat App</h1>
+        </a>
+        <nav className={styles.navigation}>
+          <ul>
+            <li>
+              <a href="/me">My Profile</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>{children}</main>
+      <footer className={styles.footer}>hello footer</footer>
+    </div>
   );
 }
 
-MainLayout.propTypes = {
+Main.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-export default MainLayout;
+export default Main;
