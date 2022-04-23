2faa60db7fdcde757e6608836b63a6cae5d577db
Jerzy Świniarski <jerzyk@codequest.com>
2022-04-23 15:02:40 +0200

    Ręczny routing + instalacja react-router-dom

--- zadanie2-chat/src/index.js
+++ zadanie2-chat/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 
 import App from './components/pages/App/App';
+import MyProfile from './components/pages/MyProfile/MyProfile';
+
+// ręczny router
+const getRoute = () => {
+  if (window.location.pathname === '/') return <App />;
+  if (window.location.pathname === '/me') return <MyProfile />;
+
+  return null;
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>{getRoute()}</React.StrictMode>,
   document.getElementById('root')
 );
