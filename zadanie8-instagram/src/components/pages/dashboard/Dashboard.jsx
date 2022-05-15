c805dcd027d2dcaeb9a7e8ad0cca7fdc497357af
Jerzy Świniarski <jerzyk@codequest.com>
2022-05-15 11:32:31 +0200

    posts observing workin'

--- zadanie8-instagram/src/components/pages/dashboard/Dashboard.jsx
+++ zadanie8-instagram/src/components/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Main from 'components/layouts/main/Main';
 import { RestrictedRoute } from 'utils/AuthorisationRoutes';
+import { observe } from 'services/firebase';
 
 function Dashboard() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    observe('posts/', setPosts);
+  }, []);
+
   return (
     <RestrictedRoute>
-      <Main>Hello from Dashboard</Main>;
+      <Main>
+        Hello from Dashboard
+        <ul>
+          {posts.map((aPost) => (
+            <li key={aPost.id}>
+              {aPost.title}: {aPost.description}
+            </li>
+          ))}
+        </ul>
+      </Main>
+      ;
     </RestrictedRoute>
   );
 }
