67536d7c175cc2605129b3722351f37d43e74961
Jerzy Świniarski <jerzyk@codequest.com>
2022-05-28 10:56:05 +0200

    Added header navigation

--- zadanie9-nextjs-github/pages/components/sections/header.js
+++ zadanie9-nextjs-github/pages/components/sections/header.js
@@ -1,5 +1,19 @@
+import Link from "next/link"
+
 export default function Header() {
   return (
-    <header>Header defined in Header.js</header>
+    <header>Header defined in Header.js
+      <nav>
+      <ul>
+        <li>
+          <Link href="/">Index</Link>
+        </li>
+        <li>
+          <Link href="/results">Go to Results</Link>
+        </li>
+      </ul>
+    </nav>
+    </header>
+    
       )
     }
