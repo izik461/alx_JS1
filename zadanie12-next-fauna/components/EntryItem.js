21202bd5e0b3f6eaedd2b86e1f5c6351f1cc2aad
Jerzy Świniarski <jerzyk@codequest.com>
2022-06-11 10:42:23 +0200

    initial next.js + fauna

---
+++ zadanie12-next-fauna/components/EntryItem.js
@@ -0,0 +1,16 @@
+import formatDate from 'date-fns/format'
+
+const EntryItem = ({ entry }) => (
+  <div className="flex flex-col space-y-2">
+    <div className="prose dark:prose-dark w-full">{entry.message}</div>
+    <div className="flex items-center space-x-3">
+      <p className="text-sm text-gray-500">{entry.name}</p>
+      <span className="text-gray-200 dark:text-gray-800">/</span>
+      <p className="text-sm text-gray-400 dark:text-gray-600">
+        {formatDate(new Date(entry.createdAt), "d MMM yyyy 'at' h:mm bb")}
+      </p>
+    </div>
+  </div>
+)
+
+ export default EntryItem
